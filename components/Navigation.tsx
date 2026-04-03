5274746bb5207ff371c1110d5890c834eba598a2
ibrasaur <ibrahim.qp.munir@gmail.com>
2026-04-03 10:31:12 +0500

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,6 @@
 'use client'
-
+import { useRef } from 'react'
+import { useMotionValue } from 'framer-motion'
 import { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
@@ -194,5 +195,4 @@ function MagneticButton({ label, onClick }: { label: string; onClick: () => void
   )
 }
 
-import { useRef } from 'react'
-import { useMotionValue } from 'framer-motion'
+
